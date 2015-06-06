c1562555af9bf273af4a431fd4838ca1e3f885c0
KazuyaMIURA <miup.0627@gmail.com>
2015-06-06 17:52:22 +0900

    add user task model

---
+++ ToMatE/UserTaskModel.swift
@@ -0,0 +1,38 @@
+//
+//  UserTaskModel.swift
+//  ToMatE
+//
+//  Created by IllyasvielVonEinzbern on 6/6/15.
+//  Copyright (c) 2015 KazuyaMIURA. All rights reserved.
+//
+
+import ObjectMapper
+
+class UserTask: Mappable {
+
+    var title: String?
+    var limitDate: NSDate?
+    var difficulty: Int?
+    var isMust: Bool?
+    var steps: Int?
+    var failureAt: NSDate?
+    var userID: Int?
+    var doneAt: NSDate?
+
+    init() {}
+
+    required init?(_ map: Map) {
+        mapping(map)
+    }
+
+    func mapping(map: Map) {
+        title <- map["title"]
+        limitDate <- map["limit_date"]
+        difficulty <- map["difficulty"]
+        steps <- map["steps"]
+        isMust <- map["is_must"]
+        failureAt <- map["failure_at"]
+        userID <- map["user_id"]
+        doneAt <- map["done_at"]
+    }
+}
